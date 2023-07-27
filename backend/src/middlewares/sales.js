cff8bdd7a38b31103d47e18b2c54df60e44e5ce5
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-07-26 22:15:00 -0300

    fix(sales): add middleware to prevent empty array payload to insert on db

--- backend/src/middlewares/sales.js
+++ backend/src/middlewares/sales.js
@@ -11,6 +11,22 @@ const saleFields = (req, res, next) => {
   next();
 };
 
+const saleFormat = (req, res, next) => {
+  const sale = req.body;
+
+  if (!Array.isArray(sale)) { 
+    return res.status(400)
+      .json({ message: 'At least one product is required to post the sale' });
+  }
+
+  if (sale.length === 0) {
+    return res.status(400)
+    .json({ message: 'At least one product is required to post the sale' });
+  }
+
+  next();
+};
+
 const updateFields = (req, res, next) => {
   if (!('quantity' in req.body)) return res.status(400).json({ message: '"quantity" is required' });
 
@@ -20,4 +36,5 @@ const updateFields = (req, res, next) => {
 module.exports = {
   saleFields,
   updateFields,
+  saleFormat,
 };
--- backend/src/routes/sales.route.js
+++ backend/src/routes/sales.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { salesController } = require('../controllers');
-const { saleFields, updateFields } = require('../middlewares/sales');
+const { saleFields, updateFields, saleFormat } = require('../middlewares/sales');
 
 const salesRoute = express.Router();
 
 salesRoute.get('/', salesController.viewSalesList);
-salesRoute.post('/', saleFields, salesController.insertSale);
+salesRoute.post('/', saleFormat, saleFields, salesController.insertSale);
 salesRoute.get('/:id', salesController.viewSaleById);
 salesRoute.put(
   '/:saleId/products/:productId/quantity', 
